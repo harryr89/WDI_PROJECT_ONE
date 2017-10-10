7e0e074097fb6ae4e8b3fe4b1d407f375fdd387f
harryr89 <harry.robertson.harry@gmail.com>
2017-10-10 11:53:14 +0100

    third push

--- js/app.js
+++ js/app.js
@@ -10,30 +10,55 @@
 
 // once dom has been fully loaded, execute the setup function.
 $(setup);
-
+  console.log('1');
 let $lis;
-
+  console.log('2');
 function setup() {
+  console.log('3');
   $lis = $('li');
-  pickRandomSquare();
+  console.log('4');
+
+console.log('pickRandomSquare');  pickRandomSquare(console.log('argument'));
+  console.log('5');
 }
 
 function pickRandomSquare() {
+  console.log('6');
   let selectedSquare = $lis[Math.floor(Math.random()*$lis.length)];
-  addSpotToSelectedSquare(selectedSquare);
+  // test (console.log('called hello'));
+  console.log('7');
+  console.log('8 addSpotToSelectedSquare'); addSpotToSelectedSquare(selectedSquare);
+  console.log('8');
 }
 
 function addSpotToSelectedSquare(square) {
+  console.log('9');
   $(square).addClass('selected').on('click', function(){
+  console.log('10');
 
-
+//begin another interation point to pickRandomSquare
 
   });
+
   // click
 
 
   setTimeout(function() {
+    console.log('11');
     $(square).removeClass("selected");
-    // dont want to click
-  }, 1000);
+    console.log('12');
+
+    //called for a loop
+    console.log('called addSpotToLoop');  addSpotToSelectedSquare();
+
+    //reduce timer by 100 every five iterations. pass add spot to selectedSquare
+
+  }, 3000 /* ($(square) - 100)*/);
+    console.log('13');
+  //take X iteration of square and reudce by reduction const
+}
+
+//test function
+function test (){
+  console.log('hello');
 }
