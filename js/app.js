2aa2a4a53acceaa144780f3cb3f7444ff68cc47d
harryr89 <harry.robertson.harry@gmail.com>
2017-10-11 14:55:37 +0100

    push eight

--- js/app.js
+++ js/app.js
@@ -12,15 +12,21 @@
 $(setup);
 
 let score = 0; //this will increment as the player is advarncing
-
+let time = 2000;
 let lives = 3; //if this gets to zero the game will end
 let $lis; //the specific ids of each square
 let clickIsSuccessful = false;
 
 function setup() {
   $lis = $('li');
+  // only run this function when user clicks on START
   pickRandomSquare();
 }
+$(document).ready(function(){
+    $("button").click(function(){
+        $("h1").hide(1000);
+    });
+});
 //provides a random number (selectedSquare) that is passed for a calss to be added
 function pickRandomSquare() {
   let selectedSquare = $lis[Math.floor(Math.random()*$lis.length)];
@@ -33,11 +39,12 @@ function addSpotToSelectedSquare(square) {
 
   $('.selected').one('click', function(){ //listening for a click and limiting it to one
     score++; //incrementing score if click has been heard
-    clickIsSuccessful = true; //take that click and console log it
+    time -= 100;
+    console.log(time);
     console.log(score + 'im the score!');
   });
   //included in addSpotToSelectedSquare
-  setTimeout(function() {
+  const startGame = setTimeout(function() {
 
     $(square).removeClass('selected').off('click'); //removing the past added class
 
@@ -46,14 +53,17 @@ function addSpotToSelectedSquare(square) {
       console.log(lives + 'im the lives');
       if (lives === 0) { //once they are at zero end the game
         console.log('game over');
+        clearTimeout(startGame);
+        return;
       }
+
     } else {
       clickIsSuccessful = false; //if click has been obtained
     }
-    //called for a loop
     pickRandomSquare();
-    
-  }, 2000 /* ($(square) - 100)*/);
+    //called for a loop
+
+  }, time /* ($(square) - 100)*/);
 }
 
 //test function
